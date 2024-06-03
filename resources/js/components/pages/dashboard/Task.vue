58cb21a460d1f6dc7974314ff709148a7897a70f
serge1997 <serge.gogolo12@gmail.com>
2024-06-02 22:49:52 -0300

    starting layout of task card

--- resources/js/components/pages/dashboard/Task.vue
+++ resources/js/components/pages/dashboard/Task.vue
@@ -7,10 +7,19 @@
             <div class="row p-3">
             <div class="col-md-4 card">
                 <div class="card-header border-0 bg-white p-1">
-                <small>short task description</small>
+                    <small>Task short title</small>
                 </div>
-                <div class="card-body border-0">
-                <small>user pics, icons</small>
+                <div class="card-body border-0 p-1">
+                    <small>user pics, icons</small>
+                </div>
+                <div class="card-footer d-flex justify-content-between border-0 bg-white p-1">
+                    <div class="w-100 d-flex gap-0">
+                        <Button class="text-warning fw-bold" text icon="pi pi-hourglass" />
+                    </div>
+                    <div class="w-100 d-flex justify-content-end gap-0">
+                        <Button text icon="pi pi-comment" />
+                        <Button text icon="pi pi-play" />
+                    </div>
                 </div>
             </div>
             </div>
