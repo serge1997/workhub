923f55dad9122039b56bf980b1b2ecb6848f6f8a
serge1997 <serge.gogolo12@gmail.com>
2024-07-22 14:28:22 -0300

    validation custom column label input. show this api response. clean this inputs after create front

--- resources/js/components/CustomColumnComponent.vue
+++ resources/js/components/CustomColumnComponent.vue
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-12 d-flex flex-column  gap-2">
                 <label for="username" class="font-semibold">Nome do campo</label>
-                <InputText id="username" class="w-100" v-model="custom.label" autocomplete="off" />
+                <InputText id="username" class="w-100" :class="invalidInput" v-model="custom.label" autocomplete="off" />
             </div>
             <div class="col-md-12 d-flex flex-column  gap-2 mt-3">
                 <label for="costumField-description" class="font-semibold">Descripção do campo</label>
@@ -25,19 +25,44 @@ export default {
             custom: {
                 label: null,
                 description: null
-            }
+            },
+            invalidInput: null,
         }
     },
     methods: {
         createCustomColumn(){
+            if(this.custom.label === null || this.custom.label === undefined){
+                this.invalidInput = "border border-danger"
+                return this.toaster("Label é obrigatorio", "error").fire();
+            }
             this.Api.post('custom-column', this.custom)
             .then(async response => {
-                console.log(response.data)
+                this.toaster(await response.data, "success").fire();
+                this.custom.description = null;
+                this.custom.label = null;
+                this.invalidInput = null;
             })
             .catch(error => {
                 console.log(error)
             })
-        }
+        },
+        toaster(response, icon){
+            const Toast = this.$swal.mixin({
+                text: response,
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                icon: icon,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = this.$swal.stopTimer;
+                    toast.onmouseleave = this.$swal.resumeTimer;
+                }
+            });
+            return Toast
+        },
+
     },
     mounted(){
     }
