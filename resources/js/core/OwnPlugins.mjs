2cfb297e2f64c1ae2b0709693b71ac2009a2cd1b
serge1997 <serge.gogolo12@gmail.com>
2024-07-25 13:12:35 -0300

    set api version configuartion back && front

--- resources/js/core/OwnPlugins.mjs
+++ resources/js/core/OwnPlugins.mjs
@@ -1,11 +1,10 @@
-import { ApiModule } from "./ApiModule.js"
 import { Container } from "./Container.js";
+
 export default {
     install(app) {
         const container = Container.app();
-        const apiInstance = container.getInstance('ApiModule');
-        app.config.globalProperties.Api = apiInstance;
         app.config.globalProperties.App = container;
-        //app.provide('api', app);
+        app.config.globalProperties.Api = container.getInstance('ApiModule');;
+        app.config.globalProperties.ApiV2 = container.getInstance('ApiModuleV2');
     }
 }
