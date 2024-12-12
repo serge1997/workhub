13ef6aa520de7f2d5cc66d7f41117ba0c6a60dd4
serge1997 <serge.gogolo12@gmail.com>
2024-12-11 23:32:04 -0300

    ajuste task list component on project view

--- resources/js/components/project/TaskListByExecutionStatusComponent.vue
+++ resources/js/components/project/TaskListByExecutionStatusComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
         <div class="col-md-12 d-flex flex-column gap-3">
-            <div v-for="(status, index) of execution_status" class="w-100 d-flex flex-column gap-2">
+            <div v-for="(status, index) of task_status_by_project" class="w-100 d-flex flex-column gap-2">
                 <div class="w-100 d-flex gap-3">
                     <Button text class="p-1 task-description" @click="listTaskByExecutionStatus(status.id, index, true)">
                         <span><i :class="`pi ${toggleIcon}`" :id="`icon_toggle_${status.id}`"></i></span>
@@ -20,7 +20,7 @@
                     <TaskListComponent
                         :tasks="tasks[index]"
                         @update-ui="listTaskByExecutionStatus(status.id, index, false)"
-                        :task-status="execution_status"
+                        :task-status="taskStatus"
                         @on-selected-task="onSelectedTask"
                     />
                 </div>
@@ -49,16 +49,17 @@ export default {
         )
     },
     props: {
-        projectId: Number
+        projectId: Number,
+        taskStatus: Object
     },
     data() {
         return {
-            execution_status: [],
             toast: useToast(),
             tasks: [],
             toggleIcon: 'pi-angle-right',
             selected_tasks_ids: [],
-            setStatusColor: statusColor
+            setStatusColor: statusColor,
+            task_status_by_project: []
         }
     },
     methods: {
@@ -92,10 +93,9 @@ export default {
         listAllTaskExecution(){
             this.Api.get('task-execution-status/list-task-by-project/' + this.projectId)
             .then(async response => {
-                this.execution_status = await response.data.data;
+                this.task_status_by_project = await response.data.data;
             })
             .catch(err => {
-                console.log(err);
                 this.toast.add({ severity: 'error', summary: 'Error', detail: "Error when loaded task status data", life: 3000 });
             })
         },
